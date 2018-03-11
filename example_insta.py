1818e81057c4f830a1fe6c52212ff0eaa7f0acf8
jeondele <36651585+jeondele@users.noreply.github.com>
2018-03-11 22:34:45 +0900

    Update example_insta.py

--- example_insta.py
+++ example_insta.py
@@ -1,7 +1,7 @@
 from instagram.client import InstagramAPI
 
-access_token = "a260613cb8cf4ea894a70bd653453295"
-client_secret = "fa908800ef484dcfb4b77e2de72923f7 "
+access_token = ""
+client_secret = " "
 api = InstagramAPI(access_token=access_token, client_secret=client_secret)
 recent_media, next_ = api.user_recent_media(user_id="adele", count=10)
 for media in recent_media:
